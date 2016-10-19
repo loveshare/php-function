0e36ea95a62fc6408a8b4cc2762d1a7b37747639
githubzhaoqian <qian_shuang@173.com>
2016-10-19 12:46:41 +0800

    ecshop 判断手机

---
+++ mobile/ecshop-mobile.php
@@ -0,0 +1,6 @@
+$ua = strtolower($_SERVER['HTTP_USER_AGENT']);
+$uachar = "/(nokia|sony|ericsson|mot|samsung|sgh|lg|philips|panasonic|alcatel|lenovo|cldc|midp|mobile)/i";
+if($ua == '' || preg_match($uachar, $ua))
+{
+  //
+}
